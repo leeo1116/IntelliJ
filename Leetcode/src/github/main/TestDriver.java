bc4d8e12b8d50cca336a7eac189418174b28e821
Liang Li <leeo1116@gmail.com>
2016-07-17 22:54:02 -0700

    07/17

--- Leetcode/src/github/main/TestDriver.java
+++ Leetcode/src/github/main/TestDriver.java
@@ -1,8 +1,9 @@
 package github.main;
 
 import github.algorithms._207_course_schedule.Solution;
+import github.algorithms._282_expression_add_operations.Solution282;
 
-public class Main {
+public class TestDriver {
 
     public static void main(String[] args) {
 	    // write your code here
@@ -10,8 +11,8 @@ public class Main {
         int [][]pres = {{1, 2}, {3, 5}, {2, 0}, {2, 8}};
         Solution s = new Solution();
         System.out.print(s.canFinish(courses, pres));
-
-
+        Solution282 s282 = new Solution282();
+        System.out.println(s282.addOperatioins("123", 6));
     }
 
 }
